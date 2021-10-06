54e659e3811733b7dd7fc1fe3d4416134099fe33
Tesfaye Atomsa <tesfayeatomsa@gmail.com>
2021-10-06 09:31:44 -0600

    adding router to index page

--- src/index.js
+++ src/index.js
@@ -2,12 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import {BrowserRouter as Router } from 'react-router-dom';
+
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Router>
+    <React.StrictMode>
     <App login="tenesami" />
-  </React.StrictMode>,
+  </React.StrictMode>
+  </Router>,
+  
   document.getElementById('root')
 );
 
